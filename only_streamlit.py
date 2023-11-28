2b8f3d82a0a644a215f30933296445234d43e91b
IvanSemeikin <149505658+IvanSemeikin@users.noreply.github.com>
2023-11-28 16:08:43 +0300

    Update only_streamlit.py

---
+++ only_streamlit.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Функция для загрузки данных из файла на GitHub
+def load_data(file_path):
+    url = f'https://raw.githubusercontent.com/your_username/your_repository/main/{file_path}.csv'
+    return pd.read_csv(url)
+
+# Функция для отображения таблицы и графика
+def show_data(button_name, metric_type):
+    st.header(f"{metric_type.capitalize()} Data for {button_name}")
+
+    # Загрузка данных по продажам
+    sales_data = load_data(f"Общая_таблица_проценты_{button_name.lower()}_sales")
+    st.subheader(f"Percentage Leader vs. Category Sales")
+    st.dataframe(sales_data)
+
+    # График по продажам
+    fig_sales = px.bar(sales_data, x='Seller', y='Percentage', title=f'{metric_type.capitalize()} Sales Distribution')
+    st.plotly_chart(fig_sales)
+
+    # Таблица продавцов-лидеров по продажам
+    sellers_data_sales = load_data(f"Общая_таблица_продавцы_{button_name.lower()}_sales")
+    st.subheader(f"Top Sellers by {metric_type.capitalize()} Sales")
+    st.dataframe(sellers_data_sales)
+
+# Основной код Streamlit
+st.title("Привет! Здесь информация по продавцам OZON")
+
+# Выбор кнопки
+selected_button = st.radio("Выберите категорию:", ["fbo", "fbs", "retail", "crossboarder", "total"])
+
+# Вывод данных для продаж
+show_data(selected_button, "sales")
+
+# Вывод данных для выручки
+show_data(selected_button, "revenue")
