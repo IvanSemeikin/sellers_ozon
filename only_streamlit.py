10038c5453346b7bb74f88b4c2ac8dd10b5d652b
IvanSemeikin <149505658+IvanSemeikin@users.noreply.github.com>
2023-11-29 12:09:30 +0300

    Update only_streamlit.py

--- only_streamlit.py
+++ only_streamlit.py
@@ -12,6 +12,14 @@ def load_data(file_path):
     data = pd.read_excel(full_path)
     return data
 
+# Функция для определения таблиц для графиков
+def show_data(button_name):
+    # Таблица продавцов-лидеров по продажам
+    sellers_data_sales = load_data(f"Общая_таблица_проценты_{button_name.lower()}_sales")
+    sellers_data_revenue = load_data(f"Общая_таблица_проценты_{button_name.lower()}_revenue")
+    return sellers_data_sales, sellers_data_revenue
+
+
 # Отображение таблицы с долей лидера от всей категории
 def show_table_percentage(button_name):
     st.header(f"Доля лидера от всей категории || {button_name}")
@@ -30,8 +38,8 @@ def show_table_top_sellers(button_name, metric_type):
     st.subheader(f"Топ продавцов по {metric_type.capitalize()}")
     st.dataframe(sellers_data_sales)
     
-# Отображение графика
-def show_graph_top_sellers(button_name, metric_type):
+# Отображение графика для продаж
+def show_graph_top_sellers_sales(button_name, metric_type):
     st.header(f"График топ продавцов по {metric_type.capitalize()} || {button_name}")
 
     # Линейный график по продавцам-лидерам
@@ -44,6 +52,22 @@ def show_graph_top_sellers(button_name, metric_type):
     selected_seller_categories = st.multiselect('Выберите категории для отображения', available_seller_categories, key=multiselect_key, default=available_seller_categories)
     filtered_sellers_data_sales = melted_sellers_data_sales[melted_sellers_data_sales['Category'].isin(selected_seller_categories)]
     fig_sellers = px.line(filtered_sellers_data_sales, x='Month', y='Sales', color='Category', title=f'Top Sellers by {metric_type.capitalize()}')
+    st.plotly_chart(fig_sellers)
+
+# Отображение графика для выручки
+def show_graph_top_sellers_revenue(button_name, metric_type):
+    st.header(f"График топ продавцов по {metric_type.capitalize()} || {button_name}")
+
+    # Линейный график по продавцам-лидерам
+    melted_sellers_data_revenue = pd.melt(sellers_data_revenue, id_vars=['Category'], var_name='Month', value_name='Sales')
+    available_seller_categories = melted_sellers_data_revenue['Category'].unique()
+
+    # Генерация уникального ключа для виджета st.multiselect
+    multiselect_key = f"multiselect_{button_name}_{metric_type}"
+    
+    selected_seller_categories = st.multiselect('Выберите категории для отображения', available_seller_categories, key=multiselect_key, default=available_seller_categories)
+    filtered_sellers_data_revenue = melted_sellers_data_revenue[melted_sellers_data_revenue['Category'].isin(selected_seller_categories)]
+    fig_sellers = px.line(filtered_sellers_data_revenue, x='Month', y='Sales', color='Category', title=f'Top Sellers by {metric_type.capitalize()}')
     st.plotly_chart(fig_sellers)
 
 
@@ -61,6 +85,8 @@ show_table_percentage(selected_button)
 show_table_percentage(selected_button)
 show_table_top_sellers(selected_button, "sales")
 show_table_top_sellers(selected_button, "revenue")
+sellers_data_sales = show_data(selected_button)
+sellers_data_revenue = show_data(selected_button)
 show_graph_top_sellers(selected_button, "sales")
 show_graph_top_sellers(selected_button, "revenue")
 
